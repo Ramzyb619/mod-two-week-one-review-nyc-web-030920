0e4989f62596a25bb43a4b378b0803defa520be2
lucassherwin <lksherwin@gmail.com>
2020-04-02 16:47:50 -0400

    not working

--- Rails-Seven-Seas/app/controllers/seas_controller.rb
+++ Rails-Seven-Seas/app/controllers/seas_controller.rb
@@ -1,6 +1,35 @@
 class SeasController < ApplicationController
   #define your controller actions here
 
+  def index
+    @seas = Sea.all
+  end
+
+  def edit
+    @sea = Sea.find(params[:id])
+  end
+
+  def create
+    @sea = Sea.create(sea_params)
+    redirect_to @sea
+  end
+
+  def new
+    # @sea = Sea.new(sea_params)
+    # @sea = Sea.create
+    # render 'new.html'
+
+  end
+
+  def show
+    @sea = Sea.find(params[:id])
+  end
+
+  def welcome
+
+  end
+
+
 
   private
   # In controller actions, use this private method to access sea params from forms.
@@ -9,8 +38,6 @@ class SeasController < ApplicationController
   def sea_params
     params.require(:sea).permit(:name, :temperature, :bio, :mood, :image_url, :favorite_color, :scariest_creature, :has_mermaids)
   end
-
-
   
 
 end
--- Rails-Seven-Seas/config/routes.rb
+++ Rails-Seven-Seas/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/seas', to: "seas"
+  get '/seas' => 'seas#index'
+  get '/seas/:id/edit' => 'seas#edit'
+  get '/seas/new' => 'seas#new'
+  get '/seas/:id' => 'seas#show'
+  get '/welcome' => 'seas#welcome'
 
 end
